22448b9bffcaf8a56110a391a55351ee573fec96
usuario <usuario@NOTE07>
2022-03-15 15:04:35 -0300

    Adicionar arquivos de projeto.

---
+++ RevisaoLogica3A2/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace RevisaoLogica3A2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(90, 167);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(270, 23);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Idade";
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(152, 54);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(138, 23);
+            this.txtIdade.TabIndex = 2;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(152, 99);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(138, 23);
+            this.txtPeso.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Peso";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 223);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Revisão Lógica 3A2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RevisaoLogica3A2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RevisaoLogica3A2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            if ((txtIdade.Text.Trim().ToString() == "") || (txtPeso.Text.Trim().ToString() == ""))
+            {
+                MessageBox.Show("Informe o Peso e a Idade");
+                txtIdade.Focus();
+                txtPeso.Focus();
+            }
+            else
+            {
+                // Converter para valores
+                int idade = Int32.Parse(txtIdade.Text);
+                int peso = Int32.Parse(txtPeso.Text);
+                // Verifica valores negativos
+                if ((idade < 0 ) || (peso < 0)){
+                    MessageBox.Show("Peso e Idade não podem ser menor que Zero");
+                    txtIdade.Focus();
+                    txtPeso.Focus();
+                }
+                else
+                {
+                    if ((idade >= 20) && ((peso >= 50) && (peso <= 80))){
+                        MessageBox.Show("Peso Ideal");
+
+                    } else if ((idade < 20) && ((peso >= 50) && (peso <= 90))){
+                        MessageBox.Show("Acima do Peso");
+
+                    }else
+                    {
+                        MessageBox.Show("Peso Inválido");
+                    }
+                }
+            }
+        }
+    }
+}
